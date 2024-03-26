6525f054811b6a4a32c37d252679064027fbe989
rajasekar <71640279+rajakalai@users.noreply.github.com>
2024-03-26 13:58:56 +0000

    updated the network name in the compute module

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance" "web" {
     }
   }
   network_interface {
-   subnetwork = "test-subnetwork"
+   subnetwork = "app-subnetwork"
    access_config {
       # Leave empty for dynamic public IP
     }
