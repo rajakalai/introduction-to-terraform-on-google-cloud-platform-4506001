ce89030771442a193cdba75198d1f8db3d70ec3d
rajasekar <71640279+rajakalai@users.noreply.github.com>
2024-03-26 13:54:19 +0000

    typo fix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "app-subnetwork" {
   name          = "test-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.app-network.id
+  network       = google_compute_network.test-network.id
   secondary_ip_range {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
@@ -31,7 +31,7 @@ resource "google_compute_instance" "web" {
     }
   }
   network_interface {
-   subnetwork = "app-subnetwork"
+   subnetwork = "test-subnetwork"
    access_config {
       # Leave empty for dynamic public IP
     }
