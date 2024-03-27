26f8b43e13c08e99cfa3ac8071acac34dde945f3
rajasekar <71640279+rajakalai@users.noreply.github.com>
2024-03-27 09:41:23 +0000

    added stage and prod

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ module "qa" {
     network_name = "qa"
     region = var.region
 }
+
+module "staging" {
+    source = "./module/blog"
+    app_name = "staging-blog"
+    project_id = "terraform-418108"
+    network_name = "staging"
+    region = var.region
+}
+
+module "prod" {
+    source = "./module/blog"
+    app_name = "prod-blog"
+    project_id = "terraform-418108"
+    network_name = "prod"
+    region = var.region
+}
